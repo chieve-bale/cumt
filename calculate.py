e33c9d579aa61f3ff01607cb288079cbe52f070b
balehu <balehu86@gmail.com>
2023-11-24 23:29:17 +0800

    完成原始刚度矩阵

--- calculate.py
+++ calculate.py
@@ -2,12 +2,14 @@ import numpy as np
 from math import cos,sin,pi
 ##单位统一为：长度mm，力N，应力MPa，默认E=206000MPa ，角度默认 °（逆时针为正）
 
+##杆件索引使用杆件的编码不使用节点编码！！！
+
 
 class gan_jian:##定义杆件的原始单元刚度矩阵
     
     def __init__(self,xu_hao=0,jie_dian=(0,0),E=206000,A=1,I=1,L=1,a=0):##类初始化
         self.xu_hao=xu_hao
-        self.jiie_dian=jie_dian
+        self.jie_dian=jie_dian
         self.E=E
         self.A=A
         self.I=I
@@ -52,14 +54,50 @@ class gan_jian:##定义杆件的原始单元刚度矩阵
 
     def zheng_ti_zuo_biao_xi(self):
         return self.TT().T@self.jv_bu_zuo_biao_xi()@self.TT()
+a=np.array([[168,0,0,-168,0,0],[0,8.064,20.16,0,-8.064,20.16],[0,20.16,67.2,0,-20.16,33.6],[-168,0,0,168,0,0],[0,-8.064,-20.16,0,8.064,-20.16],[0,20.16,33.6,0,-20.16,67.2]])
+b=np.array([[150.3,0,0,-150.3,0,0],[0,5.771,16.13,0,-5.771,16.13],[0,16.13,60.1,0,-16.13,30.1],[-150.3,0,0,150.3,0,0],[0,-5.771,-16.13,0,5.771,-16.13],[0,16.13,30.1,0,-16.13,60.1]])
+c=gan_jian().TT().T@b@gan_jian().TT()
 
-gj=gan_jian(I=40000000000,A=400000,E=210000,L=5590,xu_hao=1,a=0)
-t=gj.TT()
-k=gj.jv_bu_zuo_biao_xi()
-print(t.T,k,t)
-print(t.T@k@t)
+def gan_jian_list(gan_jian_shu,jie_dian):
+    for i in range(gan_jian_shu):
+        ll=[]
+        l=gan_jian(xu_hao=i,jie_dian=jie_dian,)
+        
+    return[{'gan_jian_xu_hao':0,'jie_dian':(0,1),'c':a},{'gan_jian_xu_hao':1,'jie_dian':(1,2),'c':c}]
 
-print(gj.zheng_ti_zuo_biao_xi())
+def gan_jian_jv_zhen(jie_dian_shu,gan_jian_shu,gan_jian_list):##输入节点数和杆件数
+    jv_zhen=np.zeros((jie_dian_shu*3,jie_dian_shu*3))##创建初始 结构原始刚度矩阵
+
+
+    for i in gan_jian_list:###这段代码繁杂但nb，可以实现杆端编号不连续（例如五号杆件两端节点为3，9），或者两个节点中间有n多个杆
+        for x in range(3):
+            for y in range(3):                
+                jv_zhen[i['jie_dian'][0]*3+x,i['jie_dian'][0]*3+y]=jv_zhen[i['jie_dian'][0]*3+x,i['jie_dian'][0]*3+y]+i['c'][x,y]
+        for x in range(3):
+            for y in range(3):
+                jv_zhen[i['jie_dian'][0]*3+x,i['jie_dian'][1]*3+y]=jv_zhen[i['jie_dian'][0]*3+x,i['jie_dian'][1]*3+y]+i['c'][x,y+3]
+        for x in range(3):
+            for y in range(3):                
+                jv_zhen[i['jie_dian'][1]*3+x,i['jie_dian'][0]*3+y]=jv_zhen[i['jie_dian'][1]*3+x,i['jie_dian'][0]*3+y]+i['c'][x+3,y]
+        for x in range(3):
+            for y in range(3):                
+                jv_zhen[i['jie_dian'][1]*3+x,i['jie_dian'][1]*3+y]=jv_zhen[i['jie_dian'][1]*3+x,i['jie_dian'][1]*3+y]+i['c'][x+3,y+3]
+
+
+
+    return jv_zhen##原始刚度矩阵
+
+print(gan_jian_jv_zhen(3,2,gan_jian_list))
+
+def hou_chu_li(jv_zhen):
+    pass
+    
+    
+def main():
+    pass
+
+if __name__=='__main__':
+    main
 
 
 
--- test.py
+++ test.py
@@ -1,22 +1,10 @@
-class gan_jian:
-    i=1
-    def __init__(self,i,L):
-        self.i=i
-        self.L=L
-    def ff(self,gg):  ##输入杆件代码,代码为数字字符，输出刚度矩阵。按照课本P227表格顺序
-        i=self.i
-        L=self.L
-        match gg:
-            case '00':
-                return [[i        ,0         ,0         ,-i      ,0         ,0       ],\
-                        [0        ,12*i/L/L  ,6*i/L     ,0       ,-12*i/L/L ,6*i/L    ],\
-                        [0        ,6*i/L     ,4*i       ,0       ,-6*i/L    ,2*i      ],\
-                        [-i       ,0         ,0         ,-i      ,0         ,0       ],\
-                        [0        ,-12*i/L/L ,-6*i/L    ,0       ,12*i/L/L  ,-6*i/L   ],\
-                        [0        ,6*i/L     ,2*i       ,0       ,-6*i/L    ,4*i      ]]
-            case '01':
-                return [[]]
-            case '02':
-                return [[]]
-        
-print(gan_jian(1,1).ff('00'))
+import numpy as np
+
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+b=np.pad(a,((2,5),(3,6)),'constant',constant_values=0)
+c=np.array([[1,2,3],[4,5,6],[7,8,9]])
+d=np.pad(a,((1,6),(5,4)),'constant',constant_values=0)
+print(a)
+print(b)
+print(d)
+print(a[1,2])
