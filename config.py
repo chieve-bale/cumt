3b6243cd5a336b33ddf537db7fae687f516dd929
balehu <balehu86@gmail.com>
2023-11-30 23:08:06 +0800

    更新设置

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import json
 ###在config里面加入你的设置内容
-config={'gan_jian':{'E': 210000, 'A': 400000, 'I': 40000000000,'a':0,'cos':0,'sin':0},\
+config={'gan_jian':{'E': 1000000, 'A': 1000000, 'I': 10000000000,'a':0,'cos':0,'sin':0},\
         'force':{'L':0,'a':0,'cos':0,'sin':0,'duan_dian':[0,0],'duan_dian_zhi':[0,0]}}
 
 # config={'gan_jian':{'E': 100000, 'A': 100000, 'I': 100000,'a':0,'cos':0,'sin':0},\
